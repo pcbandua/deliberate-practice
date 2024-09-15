81ad3f70b6d1bdfb99a8cf0191570fadd2e01bfa
Paulo Bandua <pcbandua@gmail.com>
2024-09-14 19:32:04 -0700

    JS Practice

---
+++ dp7.js
@@ -0,0 +1,53 @@
+console.log("hello world");
+
+// Write a program that uses a variable to store a number, then prints that number plus 10.
+
+
+var fiber = 456;
+console.log(fiber + 10);
+
+
+// Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+var mutrus = "Stitch";
+console.log(mutrus.toUpperCase());
+
+var mutrus = "Stitch";
+console.log(mutrus);
+
+// Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+var x = 5;
+var y = 10;
+console.log(x + y);
+
+
+// Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+var d = "Shit"
+// console.log(d.reverse)
+console.log(d.split("").reverse().join(""));
+
+// Write a program that uses a variable to store a number, then prints the number times 10.
+
+var f = 65;
+console.log(f * 10);
+
+// Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+var m = "Stitch";
+var k = "Bark";
+console.log(m.toUpperCase() + "" + k.toUpperCase());
+
+
+// Write a program that uses a variable to store a word, then prints the number of letters in the word.
+
+var k = "curmudgeon";
+console.log(k.length);
+
+
+// Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+var rr = 10.45;
+// console.log(rr.toPrecision());
+console.log(parseInt(rr));
+

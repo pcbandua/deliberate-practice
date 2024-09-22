b3d9a82bf1a13642deccdd46913c929c0e60d3ae
Paulo Bandua <pcbandua@gmail.com>
2024-09-21 19:56:21 -0700

    deliberate practice 13

---
+++ dp13.js
@@ -0,0 +1,42 @@
+// Write a Plant class with attributes and reader/writer methods for name, size, and price. Then write a method that prints out the attributes in a single sentence.
+
+// class Plant {
+//   constructor(plant_name, size, price) {
+//     this.plant_name = plant_name;
+//     this.size = size;
+//     this.price = price;
+//   }
+//   plant_info() {
+//     console.log(`"This" ${this.plant_name},  "has a size of" ${this.size}, "and costs" ${this.price},` )
+//   }
+// }
+
+// let plant = new Plant("Sunflower Plant", "1ft", "$25.00");
+// console.log(plant);
+// plant.plant_info();
+
+// Write a while loop that prints the word "hello" 5 times.
+
+// var word = "hello"
+// let index = 0
+
+// while (index < 5 ){
+//   console.log(word)
+//   index = index + 1
+// }
+
+// Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+let number = 0
+
+while (number < 101){
+  console.log(number)
+
+  number = number + 5
+}
+
+// Start with an array of numbers and compute the sum of all the numbers.
+// For example, [5, 10, 8, 3] becomes 26.
+
+// let numbers = [50, 34, 25, 46]
+

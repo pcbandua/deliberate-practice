85008cd494a70c17cca2b223a1e151952fd39f11
Paulo Bandua <pcbandua@gmail.com>
2024-10-12 23:34:08 -0700

    dp17

---
+++ dp17.js
@@ -0,0 +1,15 @@
+// # Start with an array of numbers and create a new array with only the numbers less than 20.
+// # For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
+
+var numbers = [45, 42, 90, 32]
+new_numbers = []
+var index = 0
+
+while (index < numbers.length) {
+  var number = numbers[index];
+  if (number < 20) {
+  new_numbers.push(number);
+  }
+  index = index + 1
+}
+console.log(new_numbers)

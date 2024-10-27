aff1752789c160310fd6c08240ecb04b1dafb8ed
Paulo Bandua <pcbandua@gmail.com>
2024-10-27 10:03:00 -0700

    dp20

--- dp20.js
+++ dp20.js
@@ -1,9 +1,13 @@
 // Start with an array of hashes and create a new array of string values from each hash's :name key.
 // For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
 
-var array = [{city: "Tacoma", population:480000}, {city: "Seattle", population:2500000}, {city: "Yakima", population:35000}]
+var cities = [{city: "Tacoma", population:480000}, {city: "Seattle", population:2500000}, {city: "Yakima", population:35000}]
 
 var index = 0
-while(index == array.length) {
-
+var new_cities = []
+while(index < cities.length) {
+var city = cities[index].city;
+new_cities.push(city)
+index++
 }
+console.log(new_cities)

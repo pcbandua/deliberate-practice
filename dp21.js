f7f1b704c95508640aef0a938fac1508715dcd03
Paulo Bandua <pcbandua@gmail.com>
2024-10-27 10:36:09 -0700

    dp21

--- dp21.js
+++ dp21.js
@@ -20,3 +20,42 @@ console.log(string_length)
 
 // Start with an array of numbers and create a new array with each number divided by 2.
 // For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
+
+// create array 
+let numbers = [45, 44, 38]
+// create empty array
+let new_numbers = []
+// create index
+index = 0
+// create while loop, declare new variable, cycle through array with each number divided by 2
+while (index < numbers.length){
+  // create variable to cycle through array
+  let number = numbers[index]/2
+  // push into new array 
+  new_numbers.push(number)
+  index++
+} 
+// print result 
+console.log(new_numbers)
+
+
+// Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
+// For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
+
+// create arry of hashes 
+var students = [{name: "Jerry", age: 45}, {name: "Antunar", age: 18}, {name: "Indira", age: 23}]
+// create new empty array
+var students_ages = []
+index = 0
+// create index 
+
+// create while loop to cycle through each hash while selecting the value of the age variable 
+while (index < students.length){
+// create new variable to do the cylcing for each has
+var ages = students[index].age * 2
+// push new vairable into empty array 
+students_ages.push(ages)
+index++
+}
+// print array 
+console.log(students_ages)
